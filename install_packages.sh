92f8a952ab7479816d0455e3d7eba46791735328
Keiku <kuroyanagi.keiichi@gmail.com>
2021-07-22 12:43:21 +0900

    add install_packages.sh

---
+++ install_packages.sh
@@ -0,0 +1,8 @@
+bash -c 'VERSION_ALIAS="3.8.6-mask-former" PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install 3.8.6'
+pip install -U pip
+pip install torch==1.8.0 torchvision==0.9.0
+python -m pip install detectron2 -f \
+  https://dl.fbaipublicfiles.com/detectron2/wheels/cu102/torch1.8/index.html
+pip install -r requirements.txt
+pip install opencv-python opencv-contrib-python
+ip install git+https://github.com/cocodataset/panopticapi.git
