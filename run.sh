6cbe0cc3e6b8f57cdefe11c8daffdf5d610b61da
Keiku <kuroyanagi.keiichi@gmail.com>
2021-08-10 14:30:32 +0900

    add training command

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+# demo
 mkdir output
 cd demo/
 python demo.py \
@@ -5,3 +6,8 @@ python demo.py \
 	     --input ../datasets/ADEChallengeData2016/release_test/testing/ADE_test_00003481.jpg ../datasets/ADEChallengeData2016/release_test/testing/ADE_test_00003482.jpg \
 	     --output ../output \
 	     --opts MODEL.WEIGHTS ../pretrained_models/model_final_d8dbeb.pkl
+
+# train
+python train_net.py \
+    --config-file configs/ade20k-150/maskformer_R50_bs16_160k.yaml \
+    --num-gpus 1 SOLVER.IMS_PER_BATCH 2 SOLVER.BASE_LR 0.0001
