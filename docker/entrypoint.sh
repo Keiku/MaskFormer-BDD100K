c5089d16948d25b2ee6c119640e1ac6a16d6cecf
Keiku <kuroyanagi.keiichi@gmail.com>
2021-08-10 14:30:57 +0900

    add docker

---
+++ docker/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+USER_ID=$(id -u)
+GROUP_ID=$(id -g)
+
+# create group
+if [ x"$GROUP_ID" != x"0" ]; then
+    groupadd -g $GROUP_ID $USER_NAME
+fi
+
+# add user
+if [ x"$USER_ID" != x"0" ]; then
+    useradd -d /home/$USER_NAME -m -s /bin/bash -u $USER_ID -g $GROUP_ID $USER_NAME
+fi
+
+# revert permission
+sudo chmod u-s /usr/sbin/useradd
+sudo chmod u-s /usr/sbin/groupadd
+
+# If you want to install the package after build
+sudo gpasswd -a docker sudo
+# sudo chown -R docker:docker /.pyenv
+
+exec $@
