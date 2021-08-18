807906700485207faefd810b25606a7fbd012156
Keiku <kuroyanagi.keiichi@gmail.com>
2021-08-18 23:36:37 +0900

    add run test command

--- run.sh
+++ run.sh
@@ -11,3 +11,9 @@ python demo.py \
 python train_net.py \
     --config-file configs/ade20k-150/maskformer_R50_bs16_160k.yaml \
     --num-gpus 1 SOLVER.IMS_PER_BATCH 2 SOLVER.BASE_LR 0.0001
+
+# test
+python train_net.py \
+       --config-file configs/ade20k-150/maskformer_R50_bs16_160k.yaml \
+       --eval-only OUTPUT_DIR output MODEL.WEIGHTS ./pretrained_models/model_final_d8dbeb.pkl
+
